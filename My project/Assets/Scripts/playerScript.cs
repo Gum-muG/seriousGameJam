9799a99ea02e64286465889b5fdf3ca97415eeff
Gum-muG <monty316.mm@gmail.com>
2026-06-23 18:14:54 -0500

    added spin, jump, dash

--- My project/Assets/Scripts/playerScript.cs
+++ My project/Assets/Scripts/playerScript.cs
@@ -5,11 +5,33 @@ using UnityEngine.Assemblies;
 public class player : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float tiltSpeed = 5f;
+    [SerializeField] private float spinSpeed = 360f;
+
+
     private bool playerMoving = false;
+
+
     Vector3 tiltVector;
     Vector3 targetTilt;
+
+
+    [SerializeField] private Transform beyblade_mesh;
+    private float spinY = 0;
+
+
+    private float verticalVelocity = 0f;
+    [SerializeField] private float gravity = 10f;
+    [SerializeField] private float jumpForce = 3f;
+    private bool grounded = true;
+
+
+    [SerializeField] private float dashSpeed = 30f;
+    [SerializeField] private float dashTime = 0.1f;
+    private bool dashing = false;
+    private float dashTimer = 0f;
+    private Vector3 dashDirection;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,47 +45,106 @@ public class player : MonoBehaviour
         playerMoving = false;
         Vector2 inputVector = new Vector2(0, 0);
         Vector3 currentTilt = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
+        spinY += spinSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.W))
         {
             inputVector.y += 1;
             playerMoving = true;
         }
+
         if (Input.GetKey(KeyCode.A))
         {
             inputVector.x -= 1;
             playerMoving = true;
         }
+
         if (Input.GetKey(KeyCode.S))
         {
             inputVector.y -= 1;
             playerMoving = true;
         }
+
         if (Input.GetKey(KeyCode.D))
         {
             inputVector.x += 1;
             playerMoving = true;
         }
+
         if (playerMoving)
         {
             targetTilt = new Vector3(tiltVector.x + inputVector.y * 20f, tiltVector.y, tiltVector.z + -inputVector.x * 20f);
-            currentTilt = Quaternion.Slerp(Quaternion.Euler(currentTilt), Quaternion.Euler(targetTilt), Time.deltaTime * tiltSpeed).eulerAngles;
-            Debug.Log(tiltVector);
+
+            currentTilt.x = Mathf.LerpAngle(currentTilt.x, targetTilt.x, Time.deltaTime * tiltSpeed);
+            currentTilt.z = Mathf.LerpAngle(currentTilt.z, targetTilt.z, Time.deltaTime * tiltSpeed);
+
             transform.localEulerAngles = currentTilt;
         }
+
         if (!playerMoving)
         {
             targetTilt = new Vector3(tiltVector.x, tiltVector.y, tiltVector.z);
-            currentTilt = Quaternion.Slerp(Quaternion.Euler(currentTilt), Quaternion.Euler(targetTilt), Time.deltaTime * tiltSpeed).eulerAngles;
+
+            currentTilt.x = Mathf.LerpAngle(currentTilt.x, targetTilt.x, Time.deltaTime * tiltSpeed);
+            currentTilt.z = Mathf.LerpAngle(currentTilt.z, targetTilt.z, Time.deltaTime * tiltSpeed);
+
             transform.localEulerAngles = currentTilt;
+        }
+
+        if (spinY >= 360f)
+        {
+            spinY -= 360f;
         }
 
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        transform.position += (moveDir * Time.deltaTime * moveSpeed);
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            verticalVelocity = jumpForce;
+            grounded = false;
+        }
 
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing && inputVector != Vector2.zero)
+        {
+            dashDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+            dashing = true;
+            dashTimer = dashTime;
+        }
+
+        verticalVelocity -= gravity * Time.deltaTime;
+
+        moveDir = new Vector3(inputVector.x, verticalVelocity, inputVector.y);
+
+        if (dashing)
+        {
+            transform.position += dashDirection * dashSpeed * Time.deltaTime;
+
+            dashTimer -= Time.deltaTime;
+
+            if (dashTimer <= 0f)
+            {
+                dashing = false;
+            }
+        }
+        else
+        {
+            transform.position += (moveDir * Time.deltaTime * moveSpeed);
+        }
+
+
+        if (transform.position.y <= 0f)
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.y = 0f;
+            transform.position = currentPosition;
+
+            verticalVelocity = 0f;
+            grounded = true;
+        }
+
+        beyblade_mesh.localEulerAngles = new Vector3(currentTilt.x -90, spinY, currentTilt.z);
+
     }
 }
