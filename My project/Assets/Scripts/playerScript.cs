af53830e37415ef393ad83f076c9308b726340e8
logngames <logngames@gmail.com>
2026-06-23 22:04:15 -0500

    CHECKPOINT

--- My project/Assets/Scripts/playerScript.cs
+++ My project/Assets/Scripts/playerScript.cs
@@ -5,76 +5,74 @@ using UnityEngine.Assemblies;
 public class player : MonoBehaviour
 {
 
-//PLAYER STATS//
-
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float tiltSpeed = 5f;
     [SerializeField] private float spinSpeed = 360f;
-    [SerializeField] private float dashSpeed = 30f;
-    [SerializeField] private float dashTime = 0.1f;
-    [SerializeField] private float jumpForce = 3f;
 
 
-//VECTORS//
-    private Vector3 tiltVector;
-    private Vector3 targetTilt;
-    private Vector3 dashDirection;
-
-
-//BOOLEANS//
     private bool playerMoving = false;
-    private bool grounded = false;
-    private bool dashing = false;
 
 
-//MISC//
+    Vector3 tiltVector;
+    Vector3 targetTilt;
+
+
     [SerializeField] private Transform beyblade_mesh;
     private float spinY = 0;
+
+
+    private float verticalVelocity = 0f;
+    [SerializeField] private float gravity = 10f;
+    [SerializeField] private float jumpForce = 3f;
+    private bool grounded = true;
+
+
+    [SerializeField] private float dashSpeed = 30f;
+    [SerializeField] private float dashTime = 0.1f;
+    private bool dashing = false;
     private float dashTimer = 0f;
-    private Rigidbody rb;
-
-
+    private Vector3 dashDirection;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        tiltVector = transform.localEulerAngles;
-        rb = GetComponent<Rigidbody>();
+        tiltVector = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
         HUD.instance.SetHealth(GameManager.instance.playerHealth.Health);
     }
 
-
+    // Update is called once per frame
     private void Update()
     {
-    //VARIABLES
-
         playerMoving = false;
-        Vector2 inputVector = Vector2.zero;
-        Vector3 currentTilt = transform.localEulerAngles;
+        Vector2 inputVector = new Vector2(0, 0);
+        Vector3 currentTilt = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
         spinY += spinSpeed * Time.deltaTime;
 
-
-    //Inputs
-//w
-        if (Input.GetKey(KeyCode.W)){
+        if (Input.GetKey(KeyCode.W))
+        {
             inputVector.y += 1;
             playerMoving = true;
         }
-//a
-        if (Input.GetKey(KeyCode.A)){
+
+        if (Input.GetKey(KeyCode.A))
+        {
             inputVector.x -= 1;
             playerMoving = true;
         }
-//s
-        if (Input.GetKey(KeyCode.S)){
+
+        if (Input.GetKey(KeyCode.S))
+        {
             inputVector.y -= 1;
             playerMoving = true;
         }
-//d
-        if (Input.GetKey(KeyCode.D)){
+
+        if (Input.GetKey(KeyCode.D))
+        {
             inputVector.x += 1;
             playerMoving = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.H))
+         if (Input.GetKeyDown(KeyCode.H))
         {
             Damage(2);
         }
@@ -82,48 +80,18 @@ public class player : MonoBehaviour
         if (playerMoving)
         {
             targetTilt = new Vector3(tiltVector.x + inputVector.y * 20f, tiltVector.y, tiltVector.z + -inputVector.x * 20f);
-        }
-        inputVector = inputVector.normalized;
 
-//jump
-        if (Input.GetKeyDown(KeyCode.Space) && grounded){
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-
-//dash
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing && inputVector != Vector2.zero){
-            dashDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
-            dashing = true;
-            dashTimer = dashTime;
-        }
-
-
-        //SLERP tilt
-
-        if (playerMoving){
-            targetTilt = new Vector3(
-                tiltVector.x + inputVector.y * 20f,
-                tiltVector.y,
-                tiltVector.z - inputVector.x * 20f);
-
-            currentTilt.x = Mathf.LerpAngle(
-                currentTilt.x,
-                targetTilt.x,
-                Time.deltaTime * tiltSpeed);
-
-            currentTilt.z = Mathf.LerpAngle(
-                currentTilt.z,
-                targetTilt.z,
-                Time.deltaTime * tiltSpeed);
+            currentTilt.x = Mathf.LerpAngle(currentTilt.x, targetTilt.x, Time.deltaTime * tiltSpeed);
+            currentTilt.z = Mathf.LerpAngle(currentTilt.z, targetTilt.z, Time.deltaTime * tiltSpeed);
 
             transform.localEulerAngles = currentTilt;
         }
 
-        if (!playerMoving){
-            targetTilt = tiltVector;
+        if (!playerMoving)
+        {
+            targetTilt = new Vector3(tiltVector.x, tiltVector.y, tiltVector.z);
 
             currentTilt.x = Mathf.LerpAngle(currentTilt.x, targetTilt.x, Time.deltaTime * tiltSpeed);
-
             currentTilt.z = Mathf.LerpAngle(currentTilt.z, targetTilt.z, Time.deltaTime * tiltSpeed);
 
             transform.localEulerAngles = currentTilt;
@@ -134,40 +102,59 @@ public class player : MonoBehaviour
             spinY -= 360f;
         }
 
+        inputVector = inputVector.normalized;
+
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        Vector3 velocity = rb.linearVelocity;
-
-        velocity.x = moveDir.x * moveSpeed;
-        velocity.z = moveDir.z * moveSpeed;
-
-        rb.linearVelocity = velocity;
-
-        if (dashing){
-            rb.AddForce(
-                dashDirection * dashSpeed,
-                ForceMode.Impulse);
-
-            dashing = false;
+    
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            verticalVelocity = jumpForce;
+            grounded = false;
         }
 
-        beyblade_mesh.localEulerAngles = new Vector3(
-            beyblade_mesh.localEulerAngles.x,
-            spinY,
-            beyblade_mesh.localEulerAngles.z);
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing && inputVector != Vector2.zero)
+        {
+            dashDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+            dashing = true;
+            dashTimer = dashTime;
+        }
 
-        Debug.Log(moveSpeed);
+        verticalVelocity -= gravity * Time.deltaTime;
+
+        moveDir = new Vector3(inputVector.x, verticalVelocity, inputVector.y);
+
+        if (dashing)
+        {
+            transform.position += dashDirection * dashSpeed * Time.deltaTime;
+
+            dashTimer -= Time.deltaTime;
+
+            if (dashTimer <= 0f)
+            {
+                dashing = false;
+            }
+        }
+        else
+        {
+            transform.position += (moveDir * Time.deltaTime * moveSpeed);
+        }
+
+
+        if (transform.position.y <= 0f)
+        {
+            Vector3 currentPosition = transform.position;
+            currentPosition.y = 0f;
+            transform.position = currentPosition;
+
+            verticalVelocity = 0f;
+            grounded = true;
+        }
+
+        beyblade_mesh.localEulerAngles = new Vector3(beyblade_mesh.localEulerAngles.x, spinY, beyblade_mesh.localEulerAngles.z);
+
     }
-
-    private void OnCollisionStay(Collision collision){
-        grounded = true;
-    }
-
-    private void OnCollisionExit(Collision collision){
-        grounded = false;
-    }
-
-    private void Damage(int damage){
+    private void Damage(int damage)
+    {
         GameManager.instance.playerHealth.Damage(damage);
         HUD.instance.SetHealth(GameManager.instance.playerHealth.Health);
     }
