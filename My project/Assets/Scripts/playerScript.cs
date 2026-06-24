fbefc2279faa0dc8f160e8989c318b60b96d99e2
Gum-muG <monty316.mm@gmail.com>
2026-06-23 22:59:37 -0500

    organized code a bit

--- My project/Assets/Scripts/playerScript.cs
+++ My project/Assets/Scripts/playerScript.cs
@@ -5,80 +5,94 @@ using UnityEngine.Assemblies;
 public class player : MonoBehaviour
 {
 
+//PLAYER STATS//
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float tiltSpeed = 5f;
     [SerializeField] private float spinSpeed = 360f;
-
-
-    private bool playerMoving = false;
-
-
-    Vector3 tiltVector;
-    Vector3 targetTilt;
-
-
-    [SerializeField] private Transform beyblade_mesh;
-    private float spinY = 0;
-
-
-    private float verticalVelocity = 0f;
     [SerializeField] private float gravity = 10f;
-    [SerializeField] private float jumpForce = 3f;
-    private bool grounded = true;
-
-
     [SerializeField] private float dashSpeed = 30f;
     [SerializeField] private float dashTime = 0.1f;
-    private bool dashing = false;
-    private float dashTimer = 0f;
+    [SerializeField] private float jumpForce = 3f;
+
+
+//VECTORS//
+    private Vector3 tiltVector;
+    private Vector3 targetTilt;
     private Vector3 dashDirection;
+
+
+//BOOLEANS//
+    private bool playerMoving = false;
+    private bool grounded = true;
+    private bool dashing = false;
+
+
+//VARIABLES//
+    private float spinY = 0;
+    private float verticalVelocity = 0f;
+    private float dashTimer = 0f;
+
+
+//MISC//
+    [SerializeField] private Transform beyblade_mesh;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+//START
     void Start()
     {
         tiltVector = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
         HUD.instance.SetHealth(GameManager.instance.playerHealth.Health);
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
+
+//UPDATE
+    private void Update(){
+    //Variables
         playerMoving = false;
         Vector2 inputVector = new Vector2(0, 0);
         Vector3 currentTilt = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
         spinY += spinSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.W))
-        {
+
+    //Inputs
+        if (Input.GetKey(KeyCode.W)){
             inputVector.y += 1;
             playerMoving = true;
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.A)){
             inputVector.x -= 1;
             playerMoving = true;
         }
 
-        if (Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.S)){
             inputVector.y -= 1;
             playerMoving = true;
         }
 
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)){
             inputVector.x += 1;
             playerMoving = true;
         }
 
-         if (Input.GetKeyDown(KeyCode.H))
-        {
+         if (Input.GetKeyDown(KeyCode.H)){
             Damage(2);
         }
 
-        if (playerMoving)
-        {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded){
+            verticalVelocity = jumpForce;
+            grounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing && inputVector != Vector2.zero){
+            dashDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+            dashing = true;
+            dashTimer = dashTime;
+        }
+
+
+    //Tilt SLERP
+        if (playerMoving){
             targetTilt = new Vector3(tiltVector.x + inputVector.y * 20f, tiltVector.y, tiltVector.z + -inputVector.x * 20f);
 
             currentTilt.x = Mathf.LerpAngle(currentTilt.x, targetTilt.x, Time.deltaTime * tiltSpeed);
@@ -86,9 +100,7 @@ public class player : MonoBehaviour
 
             transform.localEulerAngles = currentTilt;
         }
-
-        if (!playerMoving)
-        {
+        if (!playerMoving){
             targetTilt = new Vector3(tiltVector.x, tiltVector.y, tiltVector.z);
 
             currentTilt.x = Mathf.LerpAngle(currentTilt.x, targetTilt.x, Time.deltaTime * tiltSpeed);
@@ -97,34 +109,19 @@ public class player : MonoBehaviour
             transform.localEulerAngles = currentTilt;
         }
 
-        if (spinY >= 360f)
-        {
+        if (spinY >= 360f){
             spinY -= 360f;
         }
 
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-    
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            verticalVelocity = jumpForce;
-            grounded = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing && inputVector != Vector2.zero)
-        {
-            dashDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
-            dashing = true;
-            dashTimer = dashTime;
-        }
 
         verticalVelocity -= gravity * Time.deltaTime;
 
         moveDir = new Vector3(inputVector.x, verticalVelocity, inputVector.y);
 
-        if (dashing)
-        {
+        if (dashing){
             transform.position += dashDirection * dashSpeed * Time.deltaTime;
 
             dashTimer -= Time.deltaTime;
@@ -134,14 +131,12 @@ public class player : MonoBehaviour
                 dashing = false;
             }
         }
-        else
-        {
+        else{
             transform.position += (moveDir * Time.deltaTime * moveSpeed);
         }
 
 
-        if (transform.position.y <= 0f)
-        {
+        if (transform.position.y <= 0f){
             Vector3 currentPosition = transform.position;
             currentPosition.y = 0f;
             transform.position = currentPosition;
@@ -153,8 +148,7 @@ public class player : MonoBehaviour
         beyblade_mesh.localEulerAngles = new Vector3(beyblade_mesh.localEulerAngles.x, spinY, beyblade_mesh.localEulerAngles.z);
 
     }
-    private void Damage(int damage)
-    {
+    private void Damage(int damage){
         GameManager.instance.playerHealth.Damage(damage);
         HUD.instance.SetHealth(GameManager.instance.playerHealth.Health);
     }
